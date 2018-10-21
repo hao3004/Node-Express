912708cc2c6fd6b063692211cc4a68a71f96960e
122208997 <lehaohust@gmail.com>
2018-10-21 15:55:38 +0700

    Create a new user through POST method, install body-parser to read req.body

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 // start a web server have a individual port
 const port = 3000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+// for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); 
+// for parsing application/x-www-form-urlencoded
+
 const pug = require('pug');
 app.set('view engine', 'pug');
 // views = folder contains files
@@ -14,6 +20,7 @@ var users = [
     { name: 'Hao', id: 1},
     { name: 'An', id: 2} 
 ];
+// lưu trong memory, nghĩa là khi load lại server còn mình từng này ;v
 
 app.get('/', (req, res) => res.render('index', {
     name: 'Lang Hoai An' // gửi data qua đây, bên kia sử dụng = cách: #{name}
@@ -36,4 +43,19 @@ app.get('/users/search', (req, res) => {
         users: searchResults
     })
 })
+//link trong url
+app.get('/users/create', (req, res) => {
+    res.render('users/create');
+    // link trong folder
+})
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+    // res.render('users/index', {
+    //     users: users
+    // });
+    // hoặc
+    res.redirect('/users');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
