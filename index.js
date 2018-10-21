58fe6f7572a128926949199a9e4e6ac9f1c937e7
122208997 <lehaohust@gmail.com>
2018-10-21 12:54:08 +0700

    Template Engine(pug, send and use data through app.render())

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const app = express();
 // start a web server have a individual port
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const pug = require('pug');
+app.set('view engine', 'pug');
+// views = folder contains files
+app.set('views', './views');
 
-app.get('/users', (req, res) => res.send('User List'));
+app.get('/', (req, res) => res.render('index', {
+    name: 'Lang Hoai An' // gửi data qua đây, bên kia sử dụng = cách: #{name}
+}
+));
+
+app.get('/users', (req, res) => res.render('users/index', {
+    users: [
+        { name: 'Hao', id: 1},
+        { name: 'An', id: 2} 
+    ]
+}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
